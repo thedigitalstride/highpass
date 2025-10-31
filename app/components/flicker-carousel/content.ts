8a0bd110b6d5dc1678aff309b50317691cc58a67
ianhancock1970 <ian@miromedia.co.uk>
2025-10-31 15:32:15 +0000

    Update carouselImages in FlickerCarousel component to replace test images with new product showcase image; remove unused image entries for a cleaner implementation.

--- app/components/flicker-carousel/content.ts
+++ app/components/flicker-carousel/content.ts
@@ -14,38 +14,20 @@ export interface CarouselImage {
 
 export const carouselImages: CarouselImage[] = [
   {
-    src: '/test-images/image-1.svg',
+    src: '/andy-coaching-table.webp',
     alt: 'Product showcase 1',
     width: 800,
     height: 600,
   },
   {
-    src: '/test-images/image-2.svg',
+    src: '/andy-coaching-table.webp',
     alt: 'Product showcase 2',
     width: 800,
     height: 600,
   },
   {
-    src: '/test-images/image-3.svg',
+    src: '/andy-coaching-table.webp',
     alt: 'Product showcase 3',
-    width: 800,
-    height: 600,
-  },
-  {
-    src: '/test-images/image-4.svg',
-    alt: 'Product showcase 4',
-    width: 800,
-    height: 600,
-  },
-  {
-    src: '/test-images/image-5.svg',
-    alt: 'Product showcase 5',
-    width: 800,
-    height: 600,
-  },
-  {
-    src: '/test-images/image-6.svg',
-    alt: 'Product showcase 6',
     width: 800,
     height: 600,
   },
